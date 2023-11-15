76642dda142e5c6c9bf06cf048256f88b0ac7a20
Rinat Bazhanov <Bazhanov-rinat@yandex.ru>
2023-11-15 10:52:15 +0300

    movies-explorer-frontend-6

--- src/components/ProtectedRouteElement/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRouteElement = ({ element: Component, loggedIn, ...props }) => {
-  return loggedIn ? <Component {...props} /> : <Navigate to="/" replace />;
-}
-
-export default ProtectedRouteElement;
